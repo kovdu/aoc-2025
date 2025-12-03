7d1cbbda59051e0219b4454b3b0ec6d2b95892df
Koen Van Durme <koen.vandurme@gmail.com>
2025-12-03 22:15:30 +0100

    Day 3.

---
+++ lib/day03.ex
@@ -0,0 +1,93 @@
+defmodule Day03 do
+  def read_joltages(file) do
+    file
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(fn s -> String.graphemes(s) end)
+    |> Stream.map(fn a -> Enum.map(a, &String.to_integer/1) end)
+    |> Enum.to_list()
+  end
+
+  def max_joltage({x, y}, batteries) do
+    case batteries do
+      [a, b | rest] ->
+        if a > x do
+          max_joltage({a, 0}, [b | rest])
+        else
+          if a > y do
+            max_joltage({x, a}, [b | rest])
+          else
+            max_joltage({x, y}, [b | rest])
+          end
+        end
+
+      [a] ->
+        if a > y do
+          {x, a}
+        else
+          {x, y}
+        end
+
+      [] ->
+        {x, y}
+    end
+  end
+
+  def calculate_max_joltage(batteries) do
+    {x, y} = max_joltage({0, 0}, batteries)
+    10 * x + y
+  end
+
+  # Push number on list and maximize value
+  def push(n, l) do
+    case l do
+      [a | rest] -> if n >= a, do: [n | push(a, rest)], else: l
+      [] -> []
+    end
+  end
+
+  # Should be one method actually ...
+  def maximize_joltage(on, batteries) do
+    case batteries do
+      [b | rest] ->
+        if b >= List.last(on) do
+          # should remove reverse
+          new_on = push(b, on |> Enum.reverse()) |> Enum.reverse()
+          maximize_joltage(new_on, rest)
+        else
+          maximize_joltage(on, rest)
+        end
+
+      [] ->
+        on
+    end
+  end
+
+  def calculate_max_joltage_for_size(batteries, size) do
+    rev = batteries |> Enum.reverse()
+    {on, remaining} = Enum.split(rev, size)
+
+    maximize_joltage(on, remaining) |> Enum.reverse() |> Integer.undigits()
+  end
+
+  def part1 do
+    "inputs/day03.txt"
+    |> read_joltages()
+    |> Enum.map(fn b -> calculate_max_joltage(b) end)
+    |> Enum.sum()
+  end
+
+  def part1_alternative do
+    "inputs/day03.txt"
+    |> read_joltages()
+    |> Enum.map(fn b -> calculate_max_joltage_for_size(b, 2) end)
+    |> Enum.sum()
+  end
+
+  def part2 do
+    "inputs/day03.txt"
+    |> read_joltages()
+    |> Enum.map(fn b -> calculate_max_joltage_for_size(b, 12) end)
+    |> Enum.sum()
+  end
+end
---
+++ test/day_03_test.exs
@@ -0,0 +1,52 @@
+defmodule Day03Test do
+  use ExUnit.Case
+
+  test "calculate_max_joltage" do
+    assert 6767 |> Integer.digits() |> Day03.calculate_max_joltage() == 77
+    assert 67678 |> Integer.digits() |> Day03.calculate_max_joltage() == 78
+    assert 676_781 |> Integer.digits() |> Day03.calculate_max_joltage() == 81
+
+    assert "inputs/day03_sample.txt"
+           |> Day03.read_joltages()
+           |> Enum.map(fn b -> Day03.calculate_max_joltage(b) end)
+           |> Enum.sum() == 357
+  end
+
+  test "push" do
+    assert Day03.push(1, []) == []
+    assert Day03.push(1, [2]) == [2]
+    assert Day03.push(2, [1]) == [2]
+    assert Day03.push(1, [2, 3]) == [2, 3]
+    assert Day03.push(3, [1, 2]) == [3, 2]
+    assert Day03.push(5, [2, 4, 2, 1]) == [5, 4, 2, 1]
+    assert Day03.push(5, [2, 4, 2, 1]) == [5, 4, 2, 1]
+    assert Day03.push(6, [5, 4, 2, 1]) == [6, 5, 4, 2]
+  end
+
+  test "calculate_max_joltage_for_size" do
+    assert 6767 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(2) == 77
+    assert 67678 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(2) == 78
+    assert 676_781 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(2) == 81
+
+    assert 921_314 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(5) == 92314
+
+    assert 818_181_911_112_111 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(12) ==
+             888_911_112_111
+
+    assert 918_181_911_112_111 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(12) ==
+             988_911_112_111
+
+    assert 618_181_911_112_111 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(12) ==
+             881_911_112_111
+
+    assert 53421 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(4) == 5421
+
+    assert 7_152_242_222_112_122_421 |> Integer.digits() |> Day03.calculate_max_joltage_for_size(12) ==
+             754_222_222_421
+
+    assert "inputs/day03_sample.txt"
+           |> Day03.read_joltages()
+           |> Enum.map(fn b -> Day03.calculate_max_joltage_for_size(b, 12) end)
+           |> Enum.sum() == 3_121_910_778_619
+  end
+end
