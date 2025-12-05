3a55525457e8b8ea9d158e13c075204da9466daa
Koen Van Durme <koen.vandurme@gmail.com>
2025-12-05 18:25:46 +0100

    Day 5, part 2

--- lib/day05.ex
+++ lib/day05.ex
@@ -25,12 +25,30 @@ defmodule Day05 do
     |> Enum.count()
   end
 
+  def combine_ranges(remaining, processed) do
+    case remaining do
+      [] -> processed
+      [a] -> processed ++ [a]
+      [{s1, e1} = a, {s2, e2} | t] ->
+        if e1 < s2 do
+          combine_ranges([{s2, e2} | t], processed ++ [a])
+        else
+          combine_ranges([{s1, max(e1, e2)} | t], processed)
+        end
+    end
+  end
+
+  def count_fresh_ingredients_by_ranges(ranges) do
+    Enum.sort(ranges) |> combine_ranges([]) |> Enum.map(fn {s, e} -> e - s + 1 end) |> Enum.sum()
+  end
+
   def part1 do
     {range, ingredients} = read_ingredients("inputs/day05.txt")
     count_fresh_ingredients(range, ingredients)
   end
 
   def part2 do
-
+    {ranges, _} = read_ingredients("inputs/day05.txt")
+    count_fresh_ingredients_by_ranges(ranges)
   end
 end
--- test/day_05_test.exs
+++ test/day_05_test.exs
@@ -5,4 +5,9 @@ defmodule Day05Test do
     {range, ingredients} = Day05.read_ingredients("inputs/day05_sample.txt")
     assert Day05.count_fresh_ingredients(range, ingredients) == 3
   end
+
+  test "count_fresh_ingredients_by_ranges" do
+    {ranges, _} = Day05.read_ingredients("inputs/day05_sample.txt")
+    assert Day05.count_fresh_ingredients_by_ranges(ranges) == 14
+  end
 end
